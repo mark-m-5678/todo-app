7e937fdba6ac79339e5da97c0c70ebf8d87264fb
Mark Matheisl <matheislmarek@gmail.com>
2022-10-17 22:04:54 +0100

    Started working on handling user input and adding items

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 mod helpers;
 mod todo;
 extern crate termsize; // <-- Just makes it easier to get the terminal window size on any OS
@@ -5,12 +7,12 @@ extern crate termsize; // <-- Just makes it easier to get the terminal window si
 fn main() {
 	// Init
 	let mut todo_list = Vec::new();
-	
 	let term_size = if let Some(size) = termsize::get() {
 		helpers::TerminalDimensions{width: size.cols, height: size.rows}
 	} else {
 		helpers::TerminalDimensions{width: 100, height: 20}
 	};
+	let options = vec!["Add a new item", "Edit an existing item", "Delete an item", "Quit"];
 	
 	// Some mock data
 	todo_list.push(todo::TodoItem{title: "Test 1".to_string(), description: "Test description 1".to_string(), completed: false});
@@ -18,14 +20,38 @@ fn main() {
 	todo_list.push(todo::TodoItem{title: "Test 3".to_string(), description: "Test description 3".to_string(), completed: false});
 	
 	// Show the welcome message
+	print_welcome_message(&term_size);
+
+	// Print all todo items
+	print_todo_list(&todo_list, &term_size);
+	
+	print_options(&options);
+
+	let mut user_input = String::new();
+	let stdin = io::stdin();
+	stdin.read_line(&mut user_input).unwrap();
+	println!("You entered: {}", user_input);
+	// TODO: Finish this
+}
+
+fn print_welcome_message(term_size: &helpers::TerminalDimensions) {
 	clear!();
 	helpers::print_centre("Hello, welcome to ToDoApp version 3!", &term_size);
 	helpers::print_div(helpers::DividerType::Double, &term_size);
 	helpers::print_centre("... it's like before, but this time in Rust 🦀!", &term_size);
 	helpers::print_div(helpers::DividerType::Star, &term_size);
 	helpers::pause();
+}
 
-	// Print all todo items
+fn print_options(options: &Vec<&str>) {
+	print!("Please select from the following options:\n");
+
+	for (i, option) in options.iter().enumerate() {
+		println!("{}. {}", i + 1, option);
+	}
+}
+
+fn print_todo_list(todo_list: &Vec<todo::TodoItem>, term_size: &helpers::TerminalDimensions) {
 	clear!();
 	println!("You currently have {} to-do items.", todo_list.len());
 	helpers::print_div(helpers::DividerType::Double, &term_size);
@@ -33,4 +59,9 @@ fn main() {
 		println!("Title: {}, Descritpion: {}, Completed: {}", item.title, item.description, item.completed);
 		helpers::print_div(helpers::DividerType::Single, &term_size);
 	}
+}
+
+fn add_item(todo_list: &mut Vec<todo::TodoItem>, title: String, description: String) {
+	let new_item = todo::TodoItem{title, description, completed: false};
+	todo_list.push(new_item);
 }
