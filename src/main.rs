1df63928f8bbba59f052f8ad86e247c9d875dd9b
Mark Matheisl <matheislmarek@gmail.com>
2022-10-18 07:07:25 +0100

    Added menu handling and adding new items

--- src/main.rs
+++ src/main.rs
@@ -13,25 +13,48 @@ fn main() {
 		helpers::TerminalDimensions{width: 100, height: 20}
 	};
 	let options = vec!["Add a new item", "Edit an existing item", "Delete an item", "Quit"];
-	
+
 	// Some mock data
 	todo_list.push(todo::TodoItem{title: "Test 1".to_string(), description: "Test description 1".to_string(), completed: false});
 	todo_list.push(todo::TodoItem{title: "Test 2".to_string(), description: "Test description 2".to_string(), completed: false});
 	todo_list.push(todo::TodoItem{title: "Test 3".to_string(), description: "Test description 3".to_string(), completed: false});
-	
-	// Show the welcome message
+
+	// Show welcome message
 	print_welcome_message(&term_size);
 
-	// Print all todo items
-	print_todo_list(&todo_list, &term_size);
-	
-	print_options(&options);
+	// Main loop
+	loop {
+		print_todo_list(&todo_list, &term_size);
+		helpers::print_div(helpers::DividerType::Double, &term_size);
+		print_options(&options);
 
-	let mut user_input = String::new();
-	let stdin = io::stdin();
-	stdin.read_line(&mut user_input).unwrap();
-	println!("You entered: {}", user_input);
-	// TODO: Finish this
+		let mut user_input = String::new();
+		io::stdin().read_line(&mut user_input).expect("Failed to read line");
+		let choice: u8 = match user_input.trim().parse() {
+			Ok(num) => num,
+			Err(_) => {
+				println!("Please enter a number");
+				helpers::pause();
+				continue;
+			}
+		};
+
+		// Check the option is valid
+		if choice < 1 || choice > options.len() as u8 {
+			println!("Please enter a valid option");
+			helpers::pause();
+			continue;
+		}
+
+		// Run the corresponding function
+		match choice {
+			1 => add_item_submenu(&mut todo_list),
+			2 => println!("(Not yet implemented)"),
+			3 => println!("(Not yet implemented)"),
+			4 => quit(),
+			_ => continue,
+		}
+	}
 }
 
 fn print_welcome_message(term_size: &helpers::TerminalDimensions) {
@@ -56,12 +79,34 @@ fn print_todo_list(todo_list: &Vec<todo::TodoItem>, term_size: &helpers::Termina
 	println!("You currently have {} to-do items.", todo_list.len());
 	helpers::print_div(helpers::DividerType::Double, &term_size);
 	for item in todo_list {
-		println!("Title: {}, Descritpion: {}, Completed: {}", item.title, item.description, item.completed);
+		println!("{} | Title: {}, Descritpion: {}, Completed: {}", todo_list.iter().position(|x| x.title == item.title).unwrap() + 1, item.title, item.description, item.completed);
 		helpers::print_div(helpers::DividerType::Single, &term_size);
 	}
 }
 
-fn add_item(todo_list: &mut Vec<todo::TodoItem>, title: String, description: String) {
-	let new_item = todo::TodoItem{title, description, completed: false};
-	todo_list.push(new_item);
+fn add_item_submenu(todo_list: &mut Vec<todo::TodoItem>) {
+	let mut title = String::new();
+	let mut description = String::new();
+
+	println!("Please enter the title of the new item:");
+	io::stdin().read_line(&mut title).expect("Failed to read line");
+	println!("Please enter the description of the new item:");
+	io::stdin().read_line(&mut description).expect("Failed to read line");
+
+	if title.trim().is_empty() {
+		println!("Title cannot be empty");
+		helpers::pause();
+		return;
+	}
+
+	let title = title.trim().to_string();
+	let description = description.trim().to_string();
+
+	todo::add_item(todo_list, title, description);
+	println!("Item added successfully");
+}
+
+fn quit() {
+	println!("Goodbye!");
+	std::process::exit(0);
 }
--- src/todo.rs
+++ src/todo.rs
@@ -3,3 +3,8 @@ pub(crate) struct TodoItem {
 	pub description: String,
 	pub completed: bool,
 }
+
+pub(crate) fn add_item(todo_list: &mut Vec<TodoItem>, title: String, description: String) {
+	let new_item = TodoItem{title, description, completed: false};
+	todo_list.push(new_item);
+}
